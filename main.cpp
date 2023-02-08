318cdd12d068f0639f871f058fa0514cafbce2bf
michele-bettin <michele.bettin2002@gmail.com>
2023-02-08 11:44:15 +0100

    Ripristinato main.cpp

--- main.cpp
+++ main.cpp
@@ -1,17 +1,11 @@
 #include "MainWindow.h"
 
 #include <QApplication>
-#include "./Modelli/Data.h"
-
 
 int main(int argc, char *argv[])
 {
-    /*QApplication a(argc, argv);
+    QApplication a(argc, argv);
     MainWindow w;
     w.show();
-    return a.exec();*/
-    Data* d = new Data;
-    std::cout << d->ToString();
-    d->Avvia(-12);
-    std::cout << d->ToString();
+    return a.exec();
 }
