d59b3fcd391961b05653b86ed0ccc2a643bbaf04
michele-bettin <michele.bettin2002@gmail.com>
2023-02-11 11:50:51 +0100

    Versione di parser teoricamente funzionante

--- Parser/MioParserJSON.h
+++ Parser/MioParserJSON.h
@@ -14,16 +14,16 @@
 class MioParserJSON {
 public:
 
-    void static scriviSvegliaWidgetToJson(QJsonObject& jsonObject, const SvegliaWidget* sveglia)
+    void static scriviDaSvegliaWidgetAJSON(QJsonObject& jsonObject, SvegliaWidget* sveglia)
     {
-        jsonObject["titolo"] = sveglia -> getTitolo();
-        jsonObject["ore"] = sveglia -> getOre();
-        jsonObject["minuti"] = sveglia -> getMinuti();
-        jsonObject["secondi"] = sveglia -> getSecondi();
+        jsonObject["titolo"] = sveglia->getTitolo();
+        jsonObject["ore"] = sveglia->getOre();
+        jsonObject["minuti"] = sveglia->getMinuti();
+        jsonObject["secondi"] = sveglia->getSecondi();
     }
 
     template <typename T>
-    void static scriviArraySuFileJson(const QString& percorsoFile, const ArrayList<T*> &lista)
+    void static scriviArraySuFileJson(const QString& percorsoFile, ArrayList<T*> &lista)
     {
         QJsonArray jsonArray;
 
@@ -31,12 +31,10 @@ public:
             QJsonObject jsonObject;
 
             // Verifico la natura degli oggetti
-            if (typeid(*lista.Get(i)) == typeid(Sveglia*)) {
+            if (typeid(*lista.Get(i)) == typeid(SvegliaWidget*)) {
                 writeSvegliaToJson(jsonObject, *lista.Get(i));
 
             }
-            std::cout << typeid(lista.Get(i)).name();
-            std::cout << typeid(Sveglia*).name();
 
             jsonArray.append(jsonObject);
         }
@@ -53,36 +51,43 @@ public:
         file.close();
     }
 
-    template <typename T>
-    const ArrayList<T*> static caricaArrayDaFileJson(const QString& percorsoFile)
+
+    void static scriviDaJSONASvegliaWidget(QJsonObject& jsonObject, SvegliaWidget* sveglia, Orologio* oA = 0)
     {
-        QJsonArray jsonArray;
-
-        for (int i = 0; i < lista.GetDimensione(); i++) {
-            QJsonObject jsonObject;
-
-            // Verifico la natura degli oggetti
-            if (typeid(*lista.Get(i)) == typeid(Sveglia*)) {
-                writeSvegliaToJson(jsonObject, *lista.Get(i));
-
-            }
-            std::cout << typeid(lista.Get(i)).name();
-            std::cout << typeid(Sveglia*).name();
-
-            jsonArray.append(jsonObject);
-        }
-
-        QFile file(percorsoFile);
-        if (!file.open(QIODevice::WriteOnly | QIODevice::Text)) {
-            qDebug() << "Fallito il tentativo di apertura del file: " << percorsoFile;
-            return;
-        }
-
-        QTextStream stream(&file);
-        stream << QJsonDocument(jsonArray).toJson(QJsonDocument::Indented);
-
-        file.close();
+        sveglia = new SvegliaWidget(oA, jsonObject["titolo"], jsonObject["ore"], jsonObject["minuti"], jsonObject["secondi"]);
     }
+
+
+    // Passo anche l'orologio attuale perchè mi è necessario per creare oggetti di tipo sveglia widget, gli unici
+        // realmente per il quale è stato creato il parser
+        template <typename T>
+        const ArrayList<T*> static caricaArrayDaFileJson(const QString& percorsoFile, Orologio* oA = 0)
+        {
+            ArrayList<T*> result;
+            QFile file(percorsoFile);
+            if (!file.open(QIODevice::ReadOnly | QIODevice::Text))
+            {
+                qDebug() << "Impossibile aprire il file " << percorsoFile;
+                return result;
+            }
+
+            QJsonDocument jsonDoc = QJsonDocument::fromJson(file.readAll());
+            if (!jsonDoc.isArray())
+            {
+                qDebug() << "Il file " << percorsoFile << " non contiene un array JSON valido";
+                return result;
+            }
+
+            QJsonArray jsonArray = jsonDoc.array();
+            for (int i = 0; i < jsonArray.size(); i++)
+            {
+                QJsonObject jsonObject = jsonArray[i].toObject();
+                scriviDaJSONAWidgetSveglia(jsonObject, *result.Get(i), oA)
+                result.Aggiungi(*result.Get(i));
+            }
+
+            return result;
+        }
 };
 
 #endif // MIOPARSERJSON_H
